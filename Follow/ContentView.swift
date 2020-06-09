c6e4a0adabf3d65e5776bafee08999540993f0de
Hunter Eisler <heisler98@gmail.com>
2020-06-09 08:55:40 -0500

    Initial Commit

---
+++ Follow/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Follow
+//
+//  Created by Hunter Eisler on 6/9/20.
+//  Copyright © 2020 Hunter Eisler. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
